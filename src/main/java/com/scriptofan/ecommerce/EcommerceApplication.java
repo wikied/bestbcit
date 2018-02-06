0bd2591aac742d57e044e6c6629514b78dbc0eb4
pclundaahl <pc.lundaahl@gmail.com>
2018-02-05 19:03:24 -0800

    init development branch commit

--- src/main/java/com/scriptofan/ecommerce/EcommerceApplication.java
+++ src/main/java/com/scriptofan/ecommerce/EcommerceApplication.java
@@ -3,6 +3,7 @@ package com.scriptofan.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Begin development
 @SpringBootApplication
 public class EcommerceApplication {
 
