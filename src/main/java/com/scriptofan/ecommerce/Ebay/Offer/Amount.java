5839a29bd2b92c05f9c07a22dd312b7d59e6f451
daniel <dzhangg0614@gmail.com>
2018-02-10 16:15:48 -0800

    added getter and setters for classes

--- src/main/java/com/scriptofan/ecommerce/Ebay/Offer/Amount.java
+++ src/main/java/com/scriptofan/ecommerce/Ebay/Offer/Amount.java
@@ -1,6 +1,6 @@
 package com.scriptofan.ecommerce.Ebay.Offer;
 
-// Type is sued to express amount and applicable currency
+// Used to express amount and applicable currency
 public class Amount {
 
     // Represents the dollar value in the currency specified currency
@@ -10,4 +10,20 @@ public class Amount {
     // All currency values listed are in the CurrencyCodeEnum
     // https://developer.ebay.com/api-docs/buy/feed/types/bas:CurrencyCodeEnum
     String currency;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 }
--- src/main/java/com/scriptofan/ecommerce/Ebay/Offer/PricingSummary.java
+++ src/main/java/com/scriptofan/ecommerce/Ebay/Offer/PricingSummary.java
@@ -1,7 +1,16 @@
 package com.scriptofan.ecommerce.Ebay.Offer;
 
-
+// Specifies the listing price for the product
 public class PricingSummary {
 
+    // Listing price of the offer
     Amount price;
+
+    public Amount getPrice() {
+        return price;
+    }
+
+    public void setPrice(Amount price) {
+        this.price = price;
+    }
 }
