b28f4885cdd28471efa2fe114ca11e7b56044c39
pclundaahl <pc.lundaahl@gmail.com>
2018-02-17 15:11:17 -0800

    Add comments and constructors for Ebay.InventoryItem Weight.java

--- src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/Weight.java
+++ src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/Weight.java
@@ -1,8 +1,40 @@
 package com.scriptofan.ecommerce.Ebay.InventoryItem;
 
+/**
+ * Holds details on weight for Ebay items.
+ *
+ * @author  Jason Huang
+ * @author  Patrick Charles-Lundaahl
+ * @version 1.1
+ */
 public class Weight{
-        private WeightUnitOfMeasureEnum     unit;
-        private double                      value;
+
+    /**
+     * Default weight unit to use for Ebay items.
+     */
+    public static final WeightUnitOfMeasureEnum DEFAULT_UNIT = WeightUnitOfMeasureEnum.KILOGRAM;
+
+    private WeightUnitOfMeasureEnum     unit;
+    private double                      value;
+
+    /**
+     * Default constructor.
+     */
+    public Weight() {
+        unit    = DEFAULT_UNIT;
+        value   = 0;
+    }
+
+    /**
+     * Initializer constructor.
+     *
+     * @param unit  String representing unit of measurement.
+     * @param value Amount of weight in specified unit.
+     */
+    public Weight(String unit, double value) {
+        this.unit   = WeightUnitOfMeasureEnum.getUnit(unit);
+        this.value  = value;
+    }
 
     public WeightUnitOfMeasureEnum getUnit() {
         return unit;
