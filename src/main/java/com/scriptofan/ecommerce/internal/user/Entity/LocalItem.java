c30ed1367dfe325ee4d9a0377a669a7394a843df
pclundaahl <pc.lundaahl@gmail.com>
2018-02-17 15:26:34 -0800

    Add getters and setters to LocalItem for dimension and weight attributes

--- src/main/java/com/scriptofan/ecommerce/internal/user/Entity/LocalItem.java
+++ src/main/java/com/scriptofan/ecommerce/internal/user/Entity/LocalItem.java
@@ -15,10 +15,10 @@ public class LocalItem {
     private double          height;
     private double          length;
     private double          width;
-    // private lengthUnitMeasureEnum dimensionUnit;
+    private String          dimensionUnit;
 
     private double          weight;
-    // private weightUnitEnum weightUnit;
+    private String          weightUnit;
 
     private double          price;
 
@@ -92,6 +92,54 @@ public class LocalItem {
 
     public void setImages(List<String> images) {
         images = images;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public String getDimensionUnit() {
+        return dimensionUnit;
+    }
+
+    public void setDimensionUnit(String dimensionUnit) {
+        this.dimensionUnit = dimensionUnit;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getWeightUnit() {
+        return weightUnit;
+    }
+
+    public void setWeightUnit(String weightUnit) {
+        this.weightUnit = weightUnit;
     }
 
     /**
