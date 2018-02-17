b93250d8391142e08c6b6109f2698332f1538fa5
pclundaahl <pc.lundaahl@gmail.com>
2018-02-17 13:57:42 -0800

    Add constructors and comments to class ShipToLocationAvailability.

--- src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/ShipToLocationAvailibility.java
+++ src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/ShipToLocationAvailibility.java
@@ -1,8 +1,33 @@
 package com.scriptofan.ecommerce.Ebay.InventoryItem;
 
+/**
+ * Shipping availability information for Ebay items.
+ * Container that hodls the quantity of items available
+ * to be shipped to a buyer's home address.
+ *
+ * @author  Daniel Zhang
+ * @author  Patrick Charles-Lundaahl
+ * @version 1.1
+ */
 public class ShipToLocationAvailibility {
 
     private int quantity;
+
+    /**
+     * Default constructor.
+     */
+    public ShipToLocationAvailibility() {
+        this.quantity = 0;
+    }
+
+    /**
+     * Constructor. Initializes quantity.
+     *
+     * @param quantity Desired quantity.
+     */
+    public ShipToLocationAvailibility(int quantity) {
+        this.quantity = quantity;
+    }
 
     public int getQuantity() {
         return quantity;
