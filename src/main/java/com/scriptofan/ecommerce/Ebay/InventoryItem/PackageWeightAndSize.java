72e4456405d515f6cea4c7b267b40eeb3dcf9b1e
pclundaahl <pc.lundaahl@gmail.com>
2018-02-17 15:17:12 -0800

    Add parameter-based setDimensions() method to Ebay.InventoryItem PacakgeWeightAndSize.java

--- src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/PackageWeightAndSize.java
+++ src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/PackageWeightAndSize.java
@@ -1,5 +1,12 @@
 package com.scriptofan.ecommerce.Ebay.InventoryItem;
 
+/**
+ * Represents the weight and size of an Ebay item.
+ *
+ * @author  Jason Huang
+ * @author  Patrick Charles-Lundaah
+ * @version 1.1
+ */
 public class PackageWeightAndSize {
 
     private Dimension           dimensions;
@@ -13,6 +20,18 @@ public class PackageWeightAndSize {
 
     public void setDimensions(Dimension dimensions) {
         this.dimensions = dimensions;
+    }
+
+    /**
+     * Sets dimensions.
+     *
+     * @param length Length.
+     * @param width  Width.
+     * @param height Height.
+     * @param unit   String representing the unit of measurement.
+     */
+    public void setDimensions(double length, double width, double height, String unit) {
+        this.dimensions = new Dimension(length, width, height, unit);
     }
 
     public PackageTypeEnum getPackageTypeEnum() {
