61865da5b26ea0b588a164b7aab303d09f3b8e3c
pclundaahl <pc.lundaahl@gmail.com>
2018-02-17 14:23:18 -0800

    Add constructors and comments to Ebay Dimension class

--- src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/Dimension.java
+++ src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/Dimension.java
@@ -1,11 +1,47 @@
 package com.scriptofan.ecommerce.Ebay.InventoryItem;
 
-
+/**
+ * Represents package dimensions for Ebay items.
+ *
+ * @author  Daniel Zhang
+ * @author  Patrick Charles-Lundaahl
+ * @vesrion 1.1
+ */
 public class Dimension{
-    private double              height;
-    private double              width;
     private double              length;
+    private double              width;
+    private double              height;
     LengthUnitOfMeasureEnum     unit;
+
+    /**
+     * Default constructor.
+     */
+    public Dimension() {
+    }
+
+    /**
+     * Initializer constructor. Sets the dimensions and unit.
+     *
+     * @param length Length.
+     * @param width  Width.
+     * @param height Height.
+     * @param unit   Unit of measurement.
+     */
+    public Dimension(
+            double length,
+            double width,
+            double height,
+            LengthUnitOfMeasureEnum unit) {
+
+        if (length < 0 || width < 0 || height < 0) {
+            throw new IllegalArgumentException("Dimensions must be non-negative.");
+        }
+
+        this.length = length;
+        this.width  = width;
+        this.height = height;
+        this.unit   = unit;
+    }
 
     public double getHeight() {
         return height;
