19fc1ff1985548a08e8d4b587e9ebf7820917826
pclundaahl <pc.lundaahl@gmail.com>
2018-02-17 14:03:30 -0800

    Add comments and constructors to Ebay.InventoryItem Availability class

--- src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/Availability.java
+++ src/main/java/com/scriptofan/ecommerce/Ebay/InventoryItem/Availability.java
@@ -2,10 +2,37 @@ package com.scriptofan.ecommerce.Ebay.InventoryItem;
 
 import java.util.ArrayList;
 
+/**
+ * Container for the availability information associated with Ebay
+ * items.
+ *
+ * @author  Daniel Zhang
+ * @author  Patrick Charles-Lundaahl
+ * @version 1.1
+ */
 public class Availability {
 
     private ArrayList<PickUpAtLocationAvailibility>     pickUpAtLocationAvailibilities;
     private ShipToLocationAvailibility                  shipToLocationAvailibility;
+
+    /**
+     * Default constructor. Initializes quantity to 0 and pickup availability
+     * to null.
+     */
+    public Availability() {
+        this.pickUpAtLocationAvailibilities = null;
+        this.shipToLocationAvailibility     = new ShipToLocationAvailibility();
+    }
+
+    /**
+     * Constructor. Initializes quantity.
+     *
+     * @param quantity Number of shippable items.
+     */
+    public Availability(int quantity) {
+        this.pickUpAtLocationAvailibilities = null;
+        this.shipToLocationAvailibility     = new ShipToLocationAvailibility(quantity);
+    }
 
     public ArrayList<PickUpAtLocationAvailibility> getPickUpAtLocationAvailibilities() {
         return pickUpAtLocationAvailibilities;
