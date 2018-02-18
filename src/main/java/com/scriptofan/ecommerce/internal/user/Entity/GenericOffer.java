5abd1724d06180e05d5ae8cd91bd9a9e5c5f05f6
pclundaahl <pc.lundaahl@gmail.com>
2018-02-17 16:40:08 -0800

    Create GenericOffer abstract class

---
+++ src/main/java/com/scriptofan/ecommerce/internal/user/Entity/GenericOffer.java
@@ -0,0 +1,31 @@
+package com.scriptofan.ecommerce.internal.user.Entity;
+
+/**
+ * Represents an offer on some third party retail platform.
+ *
+ * @author  Patrick Charles-Lundaahl
+ * @version 1.0
+ */
+public abstract class GenericOffer {
+
+    // The ID that is associated with a given external platform.
+    private String platformAccountId;
+
+    /**
+     * Default constructor.
+     *
+     * @param platformAccountId PlatformAccountId
+     */
+    public GenericOffer(String platformAccountId) {
+        this.platformAccountId = platformAccountId;
+    }
+
+    /**
+     * Returns the Platform Account Id associated with this offer.
+     *
+     * @return Platform Account Id associated with this offer.
+     */
+    public String getPlatformAccountId() {
+        return platformAccountId;
+    }
+}
