4206a31f08ccdb8875268b4436558b35049e943e
pclundaahl <pc.lundaahl@gmail.com>
2018-02-17 16:03:01 -0800

    Create test endpoint so we can view what our JSON objects will look like

---
+++ src/main/java/com/scriptofan/ecommerce/internal/user/EbayItemTestController.java
@@ -0,0 +1,24 @@
+package com.scriptofan.ecommerce.internal.user;
+
+import com.scriptofan.ecommerce.Ebay.InventoryItem.InventoryItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ebay-json-test")
+public class EbayItemTestController {
+
+    private InventoryService inventoryService;
+
+    public EbayItemTestController() {
+        inventoryService = new InventoryService();
+    }
+
+    @GetMapping("/item")
+    public InventoryItem getTestEbayItem() {
+        return inventoryService.getEbayItem("ITEM-0001");
+    }
+
+}
