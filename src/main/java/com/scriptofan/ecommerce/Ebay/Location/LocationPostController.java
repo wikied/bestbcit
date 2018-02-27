35c8156a7e6b8777a09e01add9683fcf8f2f6383
Jason Huang <jasonhuang16@hotmail.com>
2018-02-27 00:17:41 -0800

    working on post controller

---
+++ src/main/java/com/scriptofan/ecommerce/Ebay/Location/LocationPostController.java
@@ -0,0 +1,16 @@
+package com.scriptofan.ecommerce.Ebay.Location;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("post-location")
+public class LocationPostController {
+    private LocationPostController locationPostController;
+
+    public LocationPostController() {
+        this.locationPostController = locationPostController;
+    }
+}
