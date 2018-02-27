afc2ccf1117402ebad5bf6723dad9544c1e57ecd
pclundaahl <pc.lundaahl@gmail.com>
2018-02-26 19:56:40 -0800

    Light refactor of AccountPolicies module for better readability

--- src/main/java/com/scriptofan/ecommerce/Ebay/AccountPolicies/Entity/FulfillmentPoliciesWrapper.java
+++ src/main/java/com/scriptofan/ecommerce/Ebay/AccountPolicies/Entity/FulfillmentPoliciesWrapper.java
@@ -2,12 +2,15 @@ package com.scriptofan.ecommerce.Ebay.AccountPolicies.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+/**
+ * Wrapper class for retrieving FulfillmentPolicy info from Ebay.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class GetFulfillmentPolicies {
+public class FulfillmentPoliciesWrapper {
 
     private FulfillmentPolicy[] fulfillmentPolicies;
 
-    public GetFulfillmentPolicies() {
+    public FulfillmentPoliciesWrapper() {
     }
 
     public FulfillmentPolicy[] getFulfillmentPolicies() {
