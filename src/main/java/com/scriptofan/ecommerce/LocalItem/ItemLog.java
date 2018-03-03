2567c5594a3f38cf22140414a4050cad9c9684ba
pclundaahl <pc.lundaahl@gmail.com>
2018-03-03 14:49:45 -0800

    Create tests for ItemLog

--- src/main/java/com/scriptofan/ecommerce/LocalItem/ItemLog.java
+++ src/main/java/com/scriptofan/ecommerce/LocalItem/ItemLog.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 public class ItemLog {
 
-    List<String> log;
+    /** Used in place of any null values passed into the log. */
+    public static final String NULL_MESSAGE = "A null value was entered for this message.";
+
+    private List<String> log;
 
     /**
      * Constructor.
@@ -14,12 +17,27 @@ public class ItemLog {
         this.log = new ArrayList<String>();
     }
 
+
+
+    /**
+     * Adds the passed message to the log. If message is null, adds placeholder
+     * message and continues without exception.
+     * @param message Message to add to the log.
+     */
     public void add(String message) {
+        if (message == null) {
+            message = NULL_MESSAGE;
+        }
         this.log.add(message);
     }
 
+
+
+    /**
+     * Returns all contained logs.
+     * @return All contained logs.
+     */
     public List<String> getFullLog() {
         return this.log;
     }
-
 }
--- src/test/java/com/scriptofan/ecommerce/LocalItem/ItemLogTests.java
+++ src/test/java/com/scriptofan/ecommerce/LocalItem/ItemLogTests.java
@@ -1,5 +1,6 @@
 package com.scriptofan.ecommerce.LocalItem;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,19 +13,77 @@ import java.util.List;
 @SpringBootTest
 public class ItemLogTests {
 
-    // A new ItemLog should not return any logs
-    @Test
-    public void newItemLogShouldBeBlank() {
-        ItemLog         itemLog;
-        List<String>    returnedLogs;
+    ItemLog         itemLog;
+    List<String>    returnedLogs;
 
+    /*
+     * Initialize the ItemLog object.
+     */
+    @Before
+    public void initializeItemLog() {
         itemLog         = new ItemLog();
-        returnedLogs    = itemLog.getFullLog();
-
-        assert(returnedLogs.size() == 0);
     }
 
-    // Adding one itemLog should return that item
-    // Adding multiple items to itemLog should return the same number of items, and they should be in the same order.
+
+
+    /*
+     * A new ItemLog should not return any logs
+     */
+    @Test
+    public void newItemLogShouldBeBlank() {
+        assert(itemLog.getFullLog().size() == 0);
+    }
+
+
+
+    /*
+     * Number of log entries retrieved should equal the number of entries added.
+     */
+    @Test
+    public void logsEnteredShouldEqualNumberOfLogsOut() {
+        for (int i = 1; i <= 10; ++i) {
+            itemLog.add("actual contents not checked here");
+            assert(itemLog.getFullLog().size() == i);
+        }
+    }
+
+
+
+    /*
+     * Logs retrieved should equal the valid logs entered in terms of content and order.
+     */
+    @Test
+    public void logsEnteredShouldEqualLogsRetrieved() {
+        String[] logData = {
+                "log 1",
+                "log 2",
+                "log 3",
+                "log 4",
+                "log 5",
+                "log 6"
+        };
+
+        for (int i = 0; i < logData.length; ++i) {
+            itemLog.add(logData[i]);
+
+            returnedLogs = itemLog.getFullLog();
+            for (int j = 0; j < returnedLogs.size(); ++j) {
+                assert(returnedLogs.get(j).equals(logData[j]));
+            }
+        }
+    }
+
+
+
+
+    /*
+     * When null values are entered and retrieved, ItemLog should return a placeholder message.
+     */
+    @Test
+    public void nullMessagesStringsShouldReturnIndicator() {
+        itemLog.add(null);
+        returnedLogs = itemLog.getFullLog();
+        assert(returnedLogs.get(0).equals(ItemLog.NULL_MESSAGE));
+    }
 
 }
