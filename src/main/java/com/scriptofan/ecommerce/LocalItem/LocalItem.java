c5bdb2f009ddc5038b86e77d4811492065ebe2e5
pclundaahl <pc.lundaahl@gmail.com>
2018-03-03 13:21:24 -0800

    Implement LocalItem.java

--- src/main/java/com/scriptofan/ecommerce/LocalItem/LocalItem.java
+++ src/main/java/com/scriptofan/ecommerce/LocalItem/LocalItem.java
@@ -1,16 +1,109 @@
 package com.scriptofan.ecommerce.LocalItem;
 
+import com.scriptofan.ecommerce.Platforms.Core.Exception.RulesetCollisionException;
 import com.scriptofan.ecommerce.User.User;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.Map;
+import java.nio.channels.NotYetBoundException;
+import java.rmi.AlreadyBoundException;
+import java.util.*;
 
 public class LocalItem {
 
     private Map<String, String> fields;
-    private Offer[]             offers;
+    private Collection<Offer>   offers;
     private User                user;
     private int                 totalQuantity;
 
-    // associateUser()
+    private ItemLog             log;
+
+    /*
+     * Constructor
+     */
+    public LocalItem() {
+        this.fields = new HashMap<>();
+        this.offers = new ArrayList<Offer>();
+        this.log    = new ItemLog();
+        this.user   = null;
+    }
+
+    /*
+     * Must throw an exception if the incoming value conflicts with an
+     * already-existing entry (i.e., the keys are the same but the values
+     * are different).
+     */
+    public void addField(String key, String value) throws RulesetCollisionException {
+
+        // Make sure that the field being added to this LocalItem
+        // doesn't conflict with one that already exists.
+        if (this.fields.containsKey(key)
+                && !this.fields.get(key).equals(value)) {
+            throw new RulesetCollisionException();
+        }
+
+        this.fields.put(key, value);
+    }
+
+    public String getField(String key) {
+        return this.fields.get(key);
+    }
+
+
+
+
+
+    public void addOffer(Offer offer) {
+        this.offers.add(offer);
+    }
+
+    public Collection<Offer> getOffers() {
+        return offers;
+    }
+
+
+
+
+
+    public void associateUser(User user) throws AlreadyBoundException{
+        if (this.user == null) {
+            this.user = user;
+        } else {
+            throw new AlreadyBoundException("User " + this.user + " is already associated with this item.");
+        }
+    }
+
+    public User getUser() throws NotYetBoundException {
+        if (this.user == null) {
+            throw new NotYetBoundException();
+        }
+        return this.user;
+    }
+
+
+
+
+
+    public void setTotalQuantity(int totalQuantity) {
+        if (totalQuantity < 0) {
+            throw new IllegalArgumentException("Total quantity must not be negative");
+        }
+        this.totalQuantity = totalQuantity;
+    }
+
+    public int getTotalQuantity() {
+        return totalQuantity;
+    }
+
+
+
+
+
+    public void log(String message) {
+        this.log.add(message);
+    }
+
+    public List<String> getFullLog() {
+        return this.log.getFullLog();
+    }
 
 }
