accccd971577aace1c7db81e445196c002b3a3fb
pclundaahl <pc.lundaahl@gmail.com>
2018-03-03 17:52:18 -0800

    implement DistributionServiceTests.distributeShouldCallPublishOncePerOffer()

--- src/test/java/com/scriptofan/ecommerce/Platforms/DistributionServiceTests.java
+++ src/test/java/com/scriptofan/ecommerce/Platforms/DistributionServiceTests.java
@@ -23,7 +23,6 @@ public class DistributionServiceTests {
         this.distributionService = new DistributionService();
     }
 
-    // Distribute should call publish() for each returned platformPublishingService.
     // Distribute should feed all of the current LocalItem's fields into publish()
     // Distribute should apply only the current offer into any one publish() call
     // Distribute should return the same LocalItem back
@@ -36,33 +35,67 @@ public class DistributionServiceTests {
     @Test
     public void shouldCallGetPlatformPublishingServiceForEachOffer() {
         final boolean[] methodWasCalled = {false, false, false};
-        LocalItem   localItem       = new LocalItem();
 
+        LocalItem localItem = new LocalItem();
         localItem.addOffer(new Offer() {
-            @Override
             public PlatformPublishingService getPlatformPublishingService() {
                 methodWasCalled[0] = true;
-                return null;
-            }
-        });
+                return null; }});
         localItem.addOffer(new Offer() {
-            @Override
             public PlatformPublishingService getPlatformPublishingService() {
                 methodWasCalled[1] = true;
-                return null;
-            }
-        });
+                return null; }});
         localItem.addOffer(new Offer() {
-            @Override
             public PlatformPublishingService getPlatformPublishingService() {
                 methodWasCalled[2] = true;
-                return null;
-            }
-        });
+                return null; }});
 
         distributionService.distribute(localItem);
         assert(methodWasCalled[0] == true);
         assert(methodWasCalled[1] == true);
         assert(methodWasCalled[2] == true);
     }
+
+
+
+
+    /*
+     * Distribute should call publish() for each returned platformPublishingService.
+     */
+    @Test
+    public void distributeShouldCallPublishOncePerOffer() {
+        final boolean[] methodWasCalled = {false, false, false};
+
+        LocalItem localItem = new LocalItem();
+        localItem.addOffer(new Offer() {
+            public PlatformPublishingService getPlatformPublishingService() {
+                return new PlatformPublishingService() {
+                    public LocalItem publish(LocalItem localItem) {
+                        methodWasCalled[0] = true;
+                        return null;
+                    }};
+            }});
+        localItem.addOffer(new Offer() {
+            public PlatformPublishingService getPlatformPublishingService() {
+                return new PlatformPublishingService() {
+                    public LocalItem publish(LocalItem localItem) {
+                        methodWasCalled[1] = true;
+                        return null;
+                    }};
+            }});
+        localItem.addOffer(new Offer() {
+            public PlatformPublishingService getPlatformPublishingService() {
+                return new PlatformPublishingService() {
+                    public LocalItem publish(LocalItem localItem) {
+                        methodWasCalled[2] = true;
+                        return null;
+                    }};
+            }});
+
+        distributionService.distribute(localItem);
+        assert(methodWasCalled[0] == true);
+        assert(methodWasCalled[1] == true);
+        assert(methodWasCalled[2] == true);
+    }
+
 }
