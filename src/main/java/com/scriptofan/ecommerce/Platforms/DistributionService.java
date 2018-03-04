70167fcd23be12b01a16e327fafaa98b6ebb5020
pclundaahl <pc.lundaahl@gmail.com>
2018-03-03 16:45:48 -0800

    Create empty interface for DistributionService

--- src/main/java/com/scriptofan/ecommerce/Platforms/DistributionService.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/DistributionService.java
@@ -1,4 +1,28 @@
 package com.scriptofan.ecommerce.Platforms;
 
+import com.scriptofan.ecommerce.LocalItem.LocalItem;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
 public class DistributionService {
+
+    /*
+     * Distributes a list of items based on their offers. Returns the complete
+     * list of items, with a log of all successes, failures, and issues for each
+     * item.
+     */
+    public List<LocalItem> distribute(List<LocalItem> items) {
+        throw new NotImplementedException();
+    }
+
+    /*
+     * Distributes LocalItem based on its offers. Returns the LocalItem, with an
+     * updated log of successes, failures and issues.
+     */
+    public LocalItem distribute(LocalItem item) {
+        throw new NotImplementedException();
+    }
+
+
 }
