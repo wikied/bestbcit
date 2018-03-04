e855726c7aff12566d6c8ae36a5aef629cf980b6
pclundaahl <pc.lundaahl@gmail.com>
2018-03-03 18:15:40 -0800

    Update PublishingService.publish() methods to take (Map<String, String> fields, Offer offer) instead of LocalItem (the idea being to keep the LocalItem from being modified by the publish() method

--- src/main/java/com/scriptofan/ecommerce/Platforms/Core/PlatformPublishingService.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Core/PlatformPublishingService.java
@@ -1,11 +1,13 @@
 package com.scriptofan.ecommerce.Platforms.Core;
 
 import com.scriptofan.ecommerce.LocalItem.LocalItem;
+import com.scriptofan.ecommerce.LocalItem.Offer;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PlatformPublishingService {
 
-    LocalItem publish(LocalItem localItem);
+    LocalItem publish(final Map<String, String> fields, Offer offer);
 
 }
--- src/main/java/com/scriptofan/ecommerce/Platforms/Ebay/EbayPublisherService.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Ebay/EbayPublisherService.java
@@ -1,14 +1,16 @@
 package com.scriptofan.ecommerce.Platforms.Ebay;
 
 import com.scriptofan.ecommerce.LocalItem.LocalItem;
+import com.scriptofan.ecommerce.LocalItem.Offer;
 import com.scriptofan.ecommerce.Platforms.Core.PlatformPublishingService;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.Map;
 
 public class EbayPublisherService implements PlatformPublishingService {
     @Override
-    public LocalItem publish(LocalItem localItem) {
+    public LocalItem publish(Map<String, String> fields, Offer offer) {
         throw new NotImplementedException();
     }
 }
