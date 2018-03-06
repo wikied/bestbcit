40af559eb5c32168f6cb8e41e3c91117ccb7185c
pclundaahl <pc.lundaahl@gmail.com>
2018-03-05 20:17:38 -0800

    Comment PlatformRepository.java

--- src/main/java/com/scriptofan/ecommerce/Platforms/Core/PlatformRepository.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Core/PlatformRepository.java
@@ -1,13 +1,29 @@
 package com.scriptofan.ecommerce.Platforms.Core;
 
+import com.scriptofan.ecommerce.LocalItem.ItemBuilderRuleset;
 import com.scriptofan.ecommerce.LocalItem.ItemBuilderRulesetFactory;
 
+/**
+ * Represents the entry point into a retail platform module (i.e.,
+ * "EbayWrapper," "EtsyWrapper," etc.).
+ *
+ * PlatformRepositories are used to register your retail platform
+ * module with the application. The application then uses these
+ * to request all the necessary things it needs to run for your
+ * given retail platform.
+ */
 public interface PlatformRepository {
-
     /**
-     * Returns this Platform's ItemBuilderRulesetFactory
-     * @return
+     * Returns this Platform's ItemBuilderRulesetFactory.
+     * @return this Platform's ItemBuilderRulesetFactory.
      */
     ItemBuilderRulesetFactory getItemBuilderRulesetFactory();
 
+
+
+    /**
+     * Returns a new instance of this Platform's ItemBuilderRuleset.
+     * @return a new instance of this Platform's ItemBuilderRuleset.
+     */
+    ItemBuilderRuleset getNewItemBuilderRuleset();
 }
