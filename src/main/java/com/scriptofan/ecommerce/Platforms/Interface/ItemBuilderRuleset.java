b90143e69b1446c803ecd311ab0da160a8493171
daniel <dzhangg0614@gmail.com>
2018-03-10 11:59:25 -0800

    added NotImplementedException class

--- src/main/java/com/scriptofan/ecommerce/Platforms/Interface/ItemBuilderRuleset.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Interface/ItemBuilderRuleset.java
@@ -1,6 +1,6 @@
 package com.scriptofan.ecommerce.Platforms.Interface;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import com.scriptofan.ecommerce.Exception.NotImplementedException;
 import com.scriptofan.ecommerce.Exception.RulesetCollisionException;
 import com.scriptofan.ecommerce.Exception.RulesetViolationException;
 import com.scriptofan.ecommerce.LocalItem.LocalItem;
