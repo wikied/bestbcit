5cfcf5fe578f4b73299dcc5c6a81c53ac3c303be
daniel <dzhangg0614@gmail.com>
2018-03-10 13:05:13 -0800

    added method to get user token

--- src/main/java/com/scriptofan/ecommerce/Platforms/Ebay/EbayOffer.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Ebay/EbayOffer.java
@@ -1,22 +1,29 @@
 package com.scriptofan.ecommerce.Platforms.Ebay;
 
+import com.scriptofan.ecommerce.Platforms.Ebay.InventoryItem.InventoryItem;
 import com.scriptofan.ecommerce.Platforms.Interface.Offer;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class EbayOffer extends Offer{
 
     public void post() {
+        //createOrReplaceInventoryItem()
 
     }
 
 
     private String createOrReplaceInventoryItem(String token, String url) {
-        String             body;
-        HttpHeaders        header;
-        HttpEntity<String> request;
+        InventoryItem          body;
+        HttpHeaders            header;
+        HttpEntity<String>     request;
         ResponseEntity<String> response;
+        RestTemplate           template;
+
+
+
         return null;
     }
 
