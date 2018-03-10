0b1ce1505ff1ae935d2a45618e29319bfb1a1ee5
daniel <dzhangg0614@gmail.com>
2018-03-10 12:53:35 -0800

    added createOrReplaceInventoryItem

--- src/main/java/com/scriptofan/ecommerce/Platforms/Ebay/EbayOffer.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Ebay/EbayOffer.java
@@ -1,13 +1,23 @@
 package com.scriptofan.ecommerce.Platforms.Ebay;
 
 import com.scriptofan.ecommerce.Platforms.Interface.Offer;
-import com.scriptofan.ecommerce.Platforms.Interface.PlatformPublishingService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 
 public class EbayOffer extends Offer{
 
+    public void post() {
 
-    @Override
-    public PlatformPublishingService getPlatformPublishingService() {
+    }
+
+
+    private String createOrReplaceInventoryItem(String token, String url) {
+        String             body;
+        HttpHeaders        header;
+        HttpEntity<String> request;
+        ResponseEntity<String> response;
         return null;
     }
+
 }
