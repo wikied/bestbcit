1abfe029a35b36afa601f0f760266a9bc3e3dbea
Jason Huang <jasonhuang16@hotmail.com>
2018-03-10 12:51:34 -0800

    intial item log generator

---
+++ src/main/java/com/scriptofan/ecommerce/LocalItem/ItemLogGenerator.java
@@ -0,0 +1,18 @@
+package com.scriptofan.ecommerce.LocalItem;
+
+public class ItemLogGenerator {
+
+    /**
+     * Temporary fail message
+     */
+    private static final String failMessage = "Item failed to proceed here";
+
+    private static final String successMessage = "Item succeeded to proceed here";
+
+    public ItemLogGenerator(LocalItem localItem){
+
+    }
+
+    public String generatedMessage(){
+    }
+}
