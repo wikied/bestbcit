3234ef486f0040ce1937f5340b618d119b076449
pclundaahl <pc.lundaahl@gmail.com>
2018-03-12 16:12:24 -0700

    Add error handler

--- src/main/java/com/scriptofan/ecommerce/Platforms/Ebay/Offer/EbayPublishOffer.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Ebay/Offer/EbayPublishOffer.java
@@ -4,8 +4,16 @@ import com.scriptofan.ecommerce.Platforms.Ebay.EbayListing;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class EbayPublishOffer {
 
@@ -21,6 +29,8 @@ public class EbayPublishOffer {
         EbayListing ebayListing = null;
 
         restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new PublishOfferHandler());
+
         httpHeaders = new HttpHeaders();
         httpHeaders.set("authorization", TOKEN_PREFIX + token);
         httpHeaders.set("Content-Language", CONTENT_LANGUAGE);
@@ -40,6 +50,58 @@ public class EbayPublishOffer {
         }
 
         return ebayListing.toString();
+    }
 
+    /**
+     * Error handler.
+     */
+    public static class PublishOfferHandler
+            implements ResponseErrorHandler
+    {
+        @Override
+        public boolean hasError(ClientHttpResponse clientHttpResponse) {
+            boolean hasError;
+            try {
+                hasError = !clientHttpResponse.getStatusCode().is2xxSuccessful();
+            }
+            catch (IOException e) {
+                throw new ResourceAccessException("IO Exception reading clientHttpResponse");
+            }
+            return hasError;
+        }
+
+        @Override
+        public void handleError(ClientHttpResponse clientHttpResponse) throws IOException {
+            System.err.println("Starting publishEbayOffer.handleError()");
+
+            HttpStatus          statusCode;
+            String              statusText;
+            String              output;
+            String              line;
+            BufferedReader      bufferReader;
+            StringBuilder       stringBuilder;
+            InputStreamReader   responseBodyReader;
+
+            statusCode = clientHttpResponse.getStatusCode();
+            statusText = clientHttpResponse.getStatusText();
+
+            /* Generate debug output */
+            stringBuilder       = new StringBuilder();
+            responseBodyReader  = new InputStreamReader(clientHttpResponse.getBody());
+            bufferReader        = new BufferedReader(responseBodyReader);
+
+            while ((line = bufferReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+            output = "";
+            output += "clientHttpResponse error:\n";
+            output += statusCode + " " + statusText + "\n";
+            output += stringBuilder.toString() + "\n";
+
+            System.err.println(output);
+
+            System.err.println("Done publishEbayOffer.handleError()");
+        }
     }
 }
