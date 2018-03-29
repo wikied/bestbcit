dfb6f91ea82a66ff2b82ab6f34d0828d12e667f0
prabhsaran <prabhvirsaran@gmail.com>
2018-03-28 18:03:56 -0700

    added uploadForm.html and it display when the application is first started

--- src/main/java/com/scriptofan/ecommerce/Controllers/UploadController.java
+++ src/main/java/com/scriptofan/ecommerce/Controllers/UploadController.java
@@ -2,12 +2,14 @@ package com.scriptofan.ecommerce.Controllers;
 
 
 import com.scriptofan.ecommerce.CSVParser.StorageService;
+import groovy.lang.GrabExclude;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@RestController
+@Controller
 public class UploadController {
 
     @Autowired
@@ -16,13 +18,18 @@ public class UploadController {
     @Autowired
     private StorageService storageService;
 
+    @GetMapping("/")
+    public String displayIntialPage(){
+
+        return "uploadForm";
+    }
     /*
         The inventory CSV multipart file is uploaded through this end point.
         The multipart is saved to the upload-dir folder in the CSVParser directory
         @param  file     multipart csv file
      */
-    @PostMapping("/upload-inventory")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+    @PostMapping("/")
+    public void handleFileUpload(@RequestParam("file") MultipartFile file) {
         try {
             storageService.store(file);
             parserController.parseMultipartFile(file);
@@ -31,7 +38,8 @@ public class UploadController {
             System.err.println("Failed to upload " + file.getOriginalFilename());
         }
 
-        return file.getOriginalFilename() + " uploaded successfully";
+        System.out.println(file.getOriginalFilename() + " uploaded successfully");
+
     }
 
 }
