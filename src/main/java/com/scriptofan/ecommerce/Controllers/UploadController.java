345ce78615fb8f440a21500c0982cd09015a486d
prabhsaran <prabhvirsaran@gmail.com>
2018-03-28 18:26:26 -0700

    able to upload the csv file though the web page

--- src/main/java/com/scriptofan/ecommerce/Controllers/UploadController.java
+++ src/main/java/com/scriptofan/ecommerce/Controllers/UploadController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -29,16 +30,21 @@ public class UploadController {
         @param  file     multipart csv file
      */
     @PostMapping("/")
-    public void handleFileUpload(@RequestParam("file") MultipartFile file) {
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                 RedirectAttributes redirectAttributes) {
         try {
             storageService.store(file);
             parserController.parseMultipartFile(file);
+            redirectAttributes.addFlashAttribute("message", "You have successfully uploaded " + file.getOriginalFilename() + "!");
+
 
         } catch (Exception e) {
             System.err.println("Failed to upload " + file.getOriginalFilename());
         }
 
         System.out.println(file.getOriginalFilename() + " uploaded successfully");
+
+        return "redirect:/";
 
     }
 
