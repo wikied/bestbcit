c572f39f320016bf5c97feeaff7d753066facdea
pclundaahl <pc.lundaahl@gmail.com>
2018-04-06 17:26:27 -0700

    Comment CoreItemBuilderRuleset

--- src/main/java/com/scriptofan/ecommerce/Platforms/Core/CoreItemBuilderRuleset.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Core/CoreItemBuilderRuleset.java
@@ -6,10 +6,23 @@ import com.scriptofan.ecommerce.Platforms.Interface.ItemBuilderRuleset;
 
 import java.util.Map;
 
+/**
+ * Responsible for validating all LocalItem key fields used by the entire
+ * application, and adding them to LocalItem objects.
+ */
 public class CoreItemBuilderRuleset implements ItemBuilderRuleset {
 
     public static final String TOTAL_QUANTITY = "totalQuantity";
 
+    /**
+     * Applies this Ruleset to the passed in LocalItem, pulling data from the
+     * fields parameter.
+     *
+     * @param localItem LocalItem to add the validated fields to.
+     * @param fields Source for data to validate and add to the LocalItem.
+     * @return LocalItem that was passed in.
+     * @throws RulesetViolationException
+     */
     @Override
     public LocalItem apply(LocalItem localItem, Map<String, String> fields)
             throws  RulesetViolationException
@@ -33,6 +46,8 @@ public class CoreItemBuilderRuleset implements ItemBuilderRuleset {
         localItem.setTotalQuantity(quantityInt);
         return localItem;
     }
+
+
 
     public String toString() {
         return "CoreItemBuilderRuleset";
