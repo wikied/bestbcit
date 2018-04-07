6bd4ff1fb4dc4c85dc7830e2781140414885d959
pclundaahl <pc.lundaahl@gmail.com>
2018-04-06 17:46:16 -0700

    Comment CoreRepository

--- src/main/java/com/scriptofan/ecommerce/Platforms/Core/CoreRepository.java
+++ src/main/java/com/scriptofan/ecommerce/Platforms/Core/CoreRepository.java
@@ -4,7 +4,14 @@ import com.scriptofan.ecommerce.Platforms.Interface.ItemBuilderRuleset;
 import com.scriptofan.ecommerce.Platforms.Interface.ItemBuilderRulesetFactory;
 import com.scriptofan.ecommerce.Platforms.Interface.PlatformRepository;
 
+/**
+ * The repository, or wrapper object, that is used to associate all the
+ * core requirements for LocalItems with our application. By adding this
+ * to the application's Config file, any core features for building items
+ * will be added.
+ */
 public class CoreRepository implements PlatformRepository {
+
     @Override
     public ItemBuilderRulesetFactory getItemBuilderRulesetFactory() {
         return new CoreItemBuilderRulesetFactory();
